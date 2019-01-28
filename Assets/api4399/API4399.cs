1a97c712d30bdd2b62078b984d3ab7c13d09915f
kingBook <412126604@qq.com>
2019-01-28 12:06:59 +0800

    update 2019-01-28 12:06

--- Assets/api4399/API4399.cs
+++ Assets/api4399/API4399.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class API4399 : MonoBehaviour{
-
+	public bool enableAPI=false;
 	private static API4399 _instance;
 	public static API4399 getInstance(){
 		if(_instance==null){
@@ -22,16 +22,20 @@ public class API4399 : MonoBehaviour{
 	private static extern void canPlayAd();
 	public void canPlayAdCallback(string datas){
 	#if UNITY_WEBGL
-		string[] list=datas.Split(',');
-		bool canPlayAd=list[0]=="true";
-		int remain=int.Parse(list[1]);
-		_canPlayAdFunc(canPlayAd,remain);
+		if(enableAPI){
+			string[] list=datas.Split(',');
+			bool canPlayAd=list[0]=="true";
+			int remain=int.Parse(list[1]);
+			_canPlayAdFunc(canPlayAd,remain);
+		}
 	#endif
 	}
 	public void CanPlayAd(CanPlayAdCallback callback){
 	#if UNITY_WEBGL
-		_canPlayAdFunc=callback;
-		canPlayAd();
+		if(enableAPI){
+			_canPlayAdFunc=callback;
+			canPlayAd();
+		}
 	#endif
 	}
 
@@ -42,16 +46,20 @@ public class API4399 : MonoBehaviour{
 	private static extern void playAd();
 	public void playAdCallback(string datas){
 	#if UNITY_WEBGL
-		string[] list=datas.Split(',');
-		int code=int.Parse(list[0]);
-		string message=list[1];
-		_playAdFunc(code,message);
+		if(enableAPI){
+			string[] list=datas.Split(',');
+			int code=int.Parse(list[0]);
+			string message=list[1];
+			_playAdFunc(code,message);
+		}
 	#endif
 	}
 	public void PlayAd(PlayAdCallback callback){
 	#if UNITY_WEBGL
-		_playAdFunc=callback;
-		playAd();
+		if(enableAPI){
+			_playAdFunc=callback;
+			playAd();
+		}
 	#endif
 	}
 
@@ -60,7 +68,7 @@ public class API4399 : MonoBehaviour{
 	private static extern void share();
 	public void Share(){
 	#if UNITY_WEBGL
-		share();
+		if(enableAPI) share();
 	#endif
 	}
 
@@ -69,10 +77,9 @@ public class API4399 : MonoBehaviour{
 	private static extern bool isLogin();
 	public bool IsLogin(){
 	#if UNITY_WEBGL
-		return isLogin();
-	#else
-		return false;
+		if(enableAPI) return isLogin();
 	#endif
+		return false;
 	}
 
 
@@ -83,17 +90,21 @@ public class API4399 : MonoBehaviour{
 	private static extern void login();
 	public void loginCallback(string datas){
 	#if UNITY_WEBGL
-		string[] list=datas.Split(',');
-		string uId=list[0];
-		string userName=list[1];
-		_loginAdFunc(uId,userName);
-		_curLoginUserId=uId;
+		if(enableAPI){
+			string[] list=datas.Split(',');
+			string uId=list[0];
+			string userName=list[1];
+			_loginAdFunc(uId,userName);
+			_curLoginUserId=uId;
+		}
 	#endif
 	}
 	public void Login(LoginCallback callback){
 	#if UNITY_WEBGL
-		_loginAdFunc=callback;
-		login();
+		if(enableAPI){
+			_loginAdFunc=callback;
+			login();
+		}
 	#endif
 	}
 
@@ -101,10 +112,9 @@ public class API4399 : MonoBehaviour{
 	private static extern string getUserAvatar(string uId);
 	public string GetUserAvatar(string uId){
 	#if UNITY_WEBGL
-		return getUserAvatar(uId);
-	#else
-		return "";
+		if(enableAPI) return getUserAvatar(uId);
 	#endif
+		return null;
 	}
 
 
@@ -112,10 +122,9 @@ public class API4399 : MonoBehaviour{
 	private static extern string getUserSmallAvatar(string uId);
 	public string GetUserSmallAvatar(string uId){
 	#if UNITY_WEBGL
-		return getUserSmallAvatar(uId);
-	#else
-		return "";
+		if(enableAPI) return getUserSmallAvatar(uId);
 	#endif
+		return null;
 	}
 
 
@@ -123,10 +132,9 @@ public class API4399 : MonoBehaviour{
 	private static extern string getUserBigAvatar(string uId);
 	public string GetUserBigAvatar(string uId){
 	#if UNITY_WEBGL
-		return getUserBigAvatar(uId);
-	#else
-		return "";
+		if(enableAPI) return getUserBigAvatar(uId);
 	#endif
+		return null;
 	}
 
 
@@ -136,7 +144,7 @@ public class API4399 : MonoBehaviour{
 	private static extern void showRanking();
 	public void ShowRanking(){
 	#if UNITY_WEBGL
-		showRanking();
+		if(enableAPI) showRanking();
 	#endif
 	}
 
@@ -147,19 +155,23 @@ public class API4399 : MonoBehaviour{
 	private static extern void submitRanking(int score);
 	public void submitRankingCallback(string datas){
 	#if UNITY_WEBGL
-		string[] list=datas.Split(',');
-		int code=int.Parse(list[0]);
-		string uId=list[1];
-		string userName=list[2];
-		int historyRank=int.Parse(list[3]);
-		int historyScore=int.Parse(list[4]);
-		_submitRankingFunc(code,uId,userName,historyRank,historyScore);
+		if(enableAPI){
+			string[] list=datas.Split(',');
+			int code=int.Parse(list[0]);
+			string uId=list[1];
+			string userName=list[2];
+			int historyRank=int.Parse(list[3]);
+			int historyScore=int.Parse(list[4]);
+			_submitRankingFunc(code,uId,userName,historyRank,historyScore);
+		}
 	#endif
 	}
 	public void SubmitRanking(int score,SubmitRankingCallback callback){
 	#if UNITY_WEBGL
-		_submitRankingFunc=callback;
-		submitRanking(score);
+		if(enableAPI){
+			_submitRankingFunc=callback;
+			submitRanking(score);
+		}
 	#endif
 	}
 
@@ -177,42 +189,46 @@ public class API4399 : MonoBehaviour{
 	private static extern void getRanking();
 	public void getRankingCallback(string datas){
 	#if UNITY_WEBGL
-		string[] tmpList=datas.Split(',');
-		string[] preList=new string[4];
-		int preCharCount=0;
-		for(int i=0;i<preList.Length;i++){
-			preList[i]=tmpList[i];
-			preCharCount+=tmpList[i].Length;
-		}
-		preCharCount+=preList.Length;//加,号个数
+		if(enableAPI){
+			string[] tmpList=datas.Split(',');
+			string[] preList=new string[4];
+			int preCharCount=0;
+			for(int i=0;i<preList.Length;i++){
+				preList[i]=tmpList[i];
+				preCharCount+=tmpList[i].Length;
+			}
+			preCharCount+=preList.Length;//加,号个数
 
-		string elementsStr=datas.Substring(preCharCount);
-		string[] elementStrs=elementsStr.Split('|');
+			string elementsStr=datas.Substring(preCharCount);
+			string[] elementStrs=elementsStr.Split('|');
 
-		int code=int.Parse(preList[0]);
-		int currentPage=int.Parse(preList[1]);
-		int totalPage=int.Parse(preList[2]);
-		bool hasNext=preList[3]=="true";
+			int code=int.Parse(preList[0]);
+			int currentPage=int.Parse(preList[1]);
+			int totalPage=int.Parse(preList[2]);
+			bool hasNext=preList[3]=="true";
 
-		RankingElement[] elementList=new RankingElement[elementStrs.Length];
-		for(int i=0;i<elementStrs.Length;i++){
-			RankingElement ele=new RankingElement();
-			tmpList=elementStrs[i].Split(',');
+			RankingElement[] elementList=new RankingElement[elementStrs.Length];
+			for(int i=0;i<elementStrs.Length;i++){
+				RankingElement ele=new RankingElement();
+				tmpList=elementStrs[i].Split(',');
 
-			ele.uId=tmpList[0];
-			ele.userName=tmpList[1];
-			ele.rank=int.Parse(tmpList[2]);
-			ele.score=int.Parse(tmpList[3]);
-			elementList[i]=ele;
-		}
+				ele.uId=tmpList[0];
+				ele.userName=tmpList[1];
+				ele.rank=int.Parse(tmpList[2]);
+				ele.score=int.Parse(tmpList[3]);
+				elementList[i]=ele;
+			}
 		
-		_getRankingFunc(code,currentPage,totalPage,hasNext,elementList);
+			_getRankingFunc(code,currentPage,totalPage,hasNext,elementList);
+		}
 	#endif
 	}
 	public void GetRanking(GetRankingCallback callback){
 	#if UNITY_WEBGL
-		_getRankingFunc=callback;
-		getRanking();
+		if(enableAPI){
+			_getRankingFunc=callback;
+			getRanking();
+		}
 	#endif
 	}
 
@@ -223,19 +239,23 @@ public class API4399 : MonoBehaviour{
 	private static extern void getMyRanking();
 	public void getMyRankingCallback(string datas){
 	#if UNITY_WEBGL
-		string[] list=datas.Split(',');
-		int code=int.Parse(list[0]);
-		string uId=list[1];
-		string userName=list[2];
-		int rank=int.Parse(list[3]);
-		int score=int.Parse(list[4]);
-		_getMyRankingFunc(code,uId,userName,rank,score);
+		if(enableAPI){
+			string[] list=datas.Split(',');
+			int code=int.Parse(list[0]);
+			string uId=list[1];
+			string userName=list[2];
+			int rank=int.Parse(list[3]);
+			int score=int.Parse(list[4]);
+			_getMyRankingFunc(code,uId,userName,rank,score);
+		}
 	#endif
 	}
 	public void GetMyRanking(GetMyRankingCallback callback){
 	#if UNITY_WEBGL
-		_getMyRankingFunc=callback;
-		getMyRanking();
+		if(enableAPI){
+			_getMyRankingFunc=callback;
+			getMyRanking();
+		}
 	#endif
 	}
 
@@ -253,40 +273,44 @@ public class API4399 : MonoBehaviour{
 	private static extern void getNearRanking();
 	private void getNearRankingCallback(string datas){
 	#if UNITY_WEBGL
-		string[] tmpList=datas.Split(',');
-		string[] preList=new string[1];
-		int preCharCount=0;
-		for(int i=0;i<preList.Length;i++){
-			preList[i]=tmpList[i];
-			preCharCount+=tmpList[i].Length;
+		if(enableAPI){
+			string[] tmpList=datas.Split(',');
+			string[] preList=new string[1];
+			int preCharCount=0;
+			for(int i=0;i<preList.Length;i++){
+				preList[i]=tmpList[i];
+				preCharCount+=tmpList[i].Length;
+			}
+			preCharCount+=preList.Length;//加,号个数
+
+			string elementsStr=datas.Substring(preCharCount);
+			string[] elementStrs=elementsStr.Split('|');
+
+			int code=int.Parse(preList[0]);
+
+			NearRankingElement[] elementList=new NearRankingElement[elementStrs.Length];
+			for(int i=0;i<elementStrs.Length;i++){
+				NearRankingElement ele=new NearRankingElement();
+				tmpList=elementStrs[i].Split(',');
+
+				ele.uId=tmpList[0];
+				ele.userName=tmpList[1];
+				ele.isMe=tmpList[1]=="true";
+				ele.rank=int.Parse(tmpList[2]);
+				ele.score=int.Parse(tmpList[3]);
+				elementList[i]=ele;
+			}
+
+			_getNearRankingFunc(code,elementList);
 		}
-		preCharCount+=preList.Length;//加,号个数
-
-		string elementsStr=datas.Substring(preCharCount);
-		string[] elementStrs=elementsStr.Split('|');
-
-		int code=int.Parse(preList[0]);
-
-		NearRankingElement[] elementList=new NearRankingElement[elementStrs.Length];
-		for(int i=0;i<elementStrs.Length;i++){
-			NearRankingElement ele=new NearRankingElement();
-			tmpList=elementStrs[i].Split(',');
-
-			ele.uId=tmpList[0];
-			ele.userName=tmpList[1];
-			ele.isMe=tmpList[1]=="true";
-			ele.rank=int.Parse(tmpList[2]);
-			ele.score=int.Parse(tmpList[3]);
-			elementList[i]=ele;
-		}
-
-		_getNearRankingFunc(code,elementList);
 	#endif
 	}
 	public void GetNearRanking(GetNearRankingCallback callback){
 	#if UNITY_WEBGL
-		_getNearRankingFunc=callback;
-		getNearRanking();
+		if(enableAPI){
+			_getNearRankingFunc=callback;
+			getNearRanking();
+		}
 	#endif
 	}
 
