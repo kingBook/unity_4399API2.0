42010ab5cbb68b89bc5d52ee722f1891838cd485
kingBook <412126604@qq.com>
2024-07-07 12:33:36 +0800

    update 2024-07-07 12:33

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -56,58 +56,58 @@ public class Main : MonoBehaviour
 
 		if(GUILayout.Button("getUserAvatar",GUILayout.ExpandHeight(true))){
 			if(inst.IsLogin()){ 
-				Debug.LogFormat("getUserAvatar:{0}",inst.GetUserAvatar(inst.curLoginUserId));
+				//Debug.LogFormat("getUserAvatar:{0}",inst.GetUserAvatar(inst.curLoginUserId));
 			}
 		}
 
 		if(GUILayout.Button("getUserSmallAvatar",GUILayout.ExpandHeight(true))){
 			if(inst.IsLogin()){ 
-				Debug.LogFormat("getUserSmallAvatar:{0}",inst.GetUserSmallAvatar(inst.curLoginUserId));
+				//Debug.LogFormat("getUserSmallAvatar:{0}",inst.GetUserSmallAvatar(inst.curLoginUserId));
 			}
 		}
 
 		if(GUILayout.Button("getUserBigAvatar",GUILayout.ExpandHeight(true))){
 			if(inst.IsLogin()){ 
-				Debug.LogFormat("getUserBigAvatar:{0}",inst.GetUserBigAvatar(inst.curLoginUserId));
+				//Debug.LogFormat("getUserBigAvatar:{0}",inst.GetUserBigAvatar(inst.curLoginUserId));
 			}
 		}
 
 		if(GUILayout.Button("showRanking",GUILayout.ExpandHeight(true))){
-			inst.ShowRanking();
+			//inst.ShowRanking();
 		}
 
 		if(GUILayout.Button("submitRanking",GUILayout.ExpandHeight(true))){
 			int score=Random.Range(1,10);
 			Debug.Log("submitRanking score:"+score);
-			inst.SubmitRanking(score,(int code,string uId,string userName,int historyRank,int historyScore)=>{
-				Debug.LogFormat("code:{0},uId:{1},userName:{2},historyRank:{3},historyScore:{4}",code,uId,userName,historyRank,historyScore);
-			});
+			//inst.SubmitRanking(score,(int code,string uId,string userName,int historyRank,int historyScore)=>{
+			//	Debug.LogFormat("code:{0},uId:{1},userName:{2},historyRank:{3},historyScore:{4}",code,uId,userName,historyRank,historyScore);
+			//});
 		}
 
 		if(GUILayout.Button("getRanking",GUILayout.ExpandHeight(true))){
-			inst.GetRanking((int code,int currentPage,int totalPage,bool hasNext,RankingElement[] list)=>{
-				Debug.LogFormat("code:{0},currentPage:{1},totalPage:{2},hasNext:{3}",code,currentPage,totalPage,hasNext);
-				for(int i=0;i<list.Length;i++){
-					var ele=list[i];
-					Debug.LogFormat("Index:{0},uId:{1},userName:{2},rank:{3},score:{4}",i,ele.uId,ele.userName,ele.rank,ele.score);
-				}
-			});
+			// inst.GetRanking((int code,int currentPage,int totalPage,bool hasNext,RankingElement[] list)=>{
+			// 	Debug.LogFormat("code:{0},currentPage:{1},totalPage:{2},hasNext:{3}",code,currentPage,totalPage,hasNext);
+			// 	for(int i=0;i<list.Length;i++){
+			// 		var ele=list[i];
+			// 		Debug.LogFormat("Index:{0},uId:{1},userName:{2},rank:{3},score:{4}",i,ele.uId,ele.userName,ele.rank,ele.score);
+			// 	}
+			// });
 		}
 
 		if(GUILayout.Button("getMyRanking",GUILayout.ExpandHeight(true))){
-			inst.GetMyRanking((int code,string uId,string userName,int rank,int score)=>{
-				Debug.LogFormat("code:{0},uId:{1},userName:{2},rank:{3},score:{4}",code,uId,userName,rank,score);
-			});
+			// inst.GetMyRanking((int code,string uId,string userName,int rank,int score)=>{
+			// 	Debug.LogFormat("code:{0},uId:{1},userName:{2},rank:{3},score:{4}",code,uId,userName,rank,score);
+			// });
 		}
 
 		if(GUILayout.Button("getNearRanking",GUILayout.ExpandHeight(true))){
-			inst.GetNearRanking((int code,NearRankingElement[] list)=>{
-				Debug.LogFormat("code:{0}",code);
-				for(int i=0;i<list.Length;i++){
-					var ele=list[i];
-					Debug.LogFormat("Index:{0},uId:{1},userName:{2},isMe:{3},rank:{4},score:{5}",i,ele.uId,ele.userName,ele.isMe,ele.rank,ele.score);
-				}
-			});
+			// inst.GetNearRanking((int code,NearRankingElement[] list)=>{
+			// 	Debug.LogFormat("code:{0}",code);
+			// 	for(int i=0;i<list.Length;i++){
+			// 		var ele=list[i];
+			// 		Debug.LogFormat("Index:{0},uId:{1},userName:{2},isMe:{3},rank:{4},score:{5}",i,ele.uId,ele.userName,ele.isMe,ele.rank,ele.score);
+			// 	}
+			// });
 		}
 
 
